2760d3b2174fd829bcdefafcb40ee4fe7d968da0
Tiago Felipe <tiagofsv95@gmail.com>
2020-04-21 16:57:04 -0300

    commit delete dist

--- dist/config/auth.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-  secret: process.env.APP_SECRET, // '828099fa74a674ecdf964db818b2f37e',
-  expiresIn: 60 * 30,
-};
